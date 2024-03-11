87e8ab573c0f9dba66e2f900b9e4fab2a67798ba
tschuehly <tschuehly@outlook.com>
2024-03-11 22:53:28 +0100

    add css

--- jte-classes/gg/jte/generated/ondemand/JtespringGenerated.java
+++ jte-classes/gg/jte/generated/ondemand/JtespringGenerated.java
@@ -1,12 +1,10 @@
 package gg.jte.generated.ondemand;
-import static java.util.stream.Collectors.groupingBy;
-import com.bootiful.frontend.web.WebController.Link;
 import com.bootiful.frontend.web.WebController.SpringResources;
 public final class JtespringGenerated {
 	public static final String JTE_NAME = "spring.jte";
-	public static final int[] JTE_LINE_INFO = {0,0,1,2,3,3,3,13,13,13,15,15,15,18,18,20,20,20,20,20,20,20,20,20,20,20,20,22,22,24,24,24,24,3,3,3,3};
+	public static final int[] JTE_LINE_INFO = {0,0,1,1,1,8,8,8,10,10,10,13,13,15,15,15,15,15,15,15,15,15,15,15,15,17,17,19,19,19,19,1,1,1,1};
 	public static void render(gg.jte.html.HtmlTemplateOutput jteOutput, gg.jte.html.HtmlInterceptor jteHtmlInterceptor, SpringResources springResources) {
-		jteOutput.writeContent("\r\n<head>\r\n    <link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/sakura.css/css/sakura.css\" type=\"text/css\">\r\n</head>\r\n\r\n<h1>\r\n    Resources for Learning Spring\r\n</h1>\r\n\r\n");
+		jteOutput.writeContent("\r\n\r\n<h1>\r\n    Resources for Learning Spring\r\n</h1>\r\n\r\n");
 		for (var resource: springResources.topics().entrySet()) {
 			jteOutput.writeContent("\r\n    <h2>\r\n        ");
 			jteOutput.setContext("h2", null);
